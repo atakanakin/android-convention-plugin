d17e4310f825b44efc635170d4c14247a23dd099
Atakan Akın <atakan.akin@commencis.com>
2025-11-10 21:28:17 +0300

    chore: add .gitignore and configure Gradle wrapper properties

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,22 +1,8 @@
-pluginManagement {
+dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
     }
 }
 
